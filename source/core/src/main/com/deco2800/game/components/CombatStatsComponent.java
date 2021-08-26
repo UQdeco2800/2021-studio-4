94c7d556f38ef06c5beb175ea31c9894d8bbeb5c
Jesse Graf <jesse@grafip.com>
2021-08-26 15:07:11 +1000

    More Progress on the death screen implementaion. Exits when health 0? Need to go to death screen still

--- source/core/src/main/com/deco2800/game/components/CombatStatsComponent.java
+++ source/core/src/main/com/deco2800/game/components/CombatStatsComponent.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.deco2800.game.GdxGame;
 import com.deco2800.game.components.endgame.DeathScreenActions;
+import com.deco2800.game.components.maingame.MainGameExitDisplay;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -90,10 +91,13 @@ public class CombatStatsComponent extends Component {
   }
 
   public void hit(CombatStatsComponent attacker) {
-     /*
+
     int newHealth = getHealth() - attacker.getBaseAttack();
     setHealth(newHealth);
-     */
+    if (isDead()) {
+      MainGameExitDisplay main = new MainGameExitDisplay();
+      main.goToDeath();
+    }
     //setHealth(newHealth);
     //setScreen(GdxGame.ScreenType.MAIN_MENU);
 //    do {
@@ -101,6 +105,6 @@ public class CombatStatsComponent extends Component {
 //      setHealth(newHealth);
 //    } while (!(isDead()));
     // Add death component to the player.
-    entity.getEvents().trigger("playerDeath");
+
   }
 }
