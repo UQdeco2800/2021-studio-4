fa260fd45eb81fd015ef87db175474492f3d0ee0
Finn <f.beardmore@uqconnect.edu.au>
2021-08-30 06:48:52 +1000

    Further Test Updates
    Added Loading assets, stop music, and isMusicPlaying

--- source/core/src/test/com/deco2800/game/services/MusicServiceTest.java
+++ source/core/src/test/com/deco2800/game/services/MusicServiceTest.java
@@ -1,25 +1,53 @@
 package com.deco2800.game.services;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Graphics;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.Texture;
 import com.deco2800.game.extensions.GameExtension;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.verify;
 
 public class MusicServiceTest {
-    /*@Test
+    @Test
     void playMusic() {
-        ResourceService resourceService = ServiceLocator.getResourceService();
-        resourceService.loadMusic(new String[]{"sounds/MainMenuMusic.mp3"});
-        ServiceLocator.getResourceService().loadAll();
+      /*  String[] music1 = new String[]{"sounds/MainMenuMusic.mp3"};
+
+
+        AssetManager assetManager = spy(AssetManager.class);
+        ResourceService resourceService = new ResourceService(assetManager);
+
+        resourceService.loadMusic(music1);
+        resourceService.loadAll();
         MusicService musicService = new MusicService("sounds/MainMenuMusic.mp3");
         musicService.playMusic();
-        assertTrue(musicService.isMusicPlaying());
-    }*/
+        assertTrue(musicService.isMusicPlaying());*/
+    }
+    @Test
+    void loadAllShouldLoadUnloadAssets() {
+        String[] music1 = new String[]{"sounds/MainMenuMusic.mp3"};
+
+
+        AssetManager assetManager = spy(AssetManager.class);
+        ResourceService resourceService = new ResourceService(assetManager);
+
+        resourceService.loadMusic(music1);
+        resourceService.loadAll();
+
+
+    }
+    @Test
+    void stopMusic() {
+
+    }
+    @Test
+    void isMusicPlaying() {
+
+    }
 }
