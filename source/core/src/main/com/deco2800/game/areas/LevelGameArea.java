012d19031209dd6453a796bcf83e537e0a66d2d9
bryan-storey <88088002+bryan-storey@users.noreply.github.com>
2021-08-31 22:30:10 +1000

    Untrack obstacle entities when removed

--- source/core/src/main/com/deco2800/game/areas/LevelGameArea.java
+++ source/core/src/main/com/deco2800/game/areas/LevelGameArea.java
@@ -260,6 +260,12 @@ public class LevelGameArea extends GameArea {
     }
   }
 
+  @Override
+  public void untrackEntity(Entity entity) {
+    this.obstacleEntities.remove(entity);
+    super.untrackEntity(entity);
+  }
+
   private void spawnLevelFromFile() {
     BufferedReader reader = null;
     try {
