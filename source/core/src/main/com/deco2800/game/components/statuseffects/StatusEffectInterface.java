462034f029d6c1be9639b5f1c0e46e0f0cd837e6
Dannie Cai <dannie.xh.cai547@gmail.com>
2021-09-09 22:32:42 +1000

    Added StatusEffectEnum, interface and operation class.

---
+++ source/core/src/main/com/deco2800/game/components/statuseffects/StatusEffectInterface.java
@@ -0,0 +1,7 @@
+package com.deco2800.game.components.statuseffects;
+
+/** Handles the status changes */
+public interface StatusEffectInterface {
+    void boosts(int type, int boost, int statOriginal);
+
+}
