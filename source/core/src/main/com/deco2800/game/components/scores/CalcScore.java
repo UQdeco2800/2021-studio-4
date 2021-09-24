089066573b2c37e6b01793a21f87104498e9b371
Jesse Graf <jesse@grafip.com>
2021-09-24 12:59:39 +1000

    Added a working score method into the code which calculates a players score based off of how many seconds he has left to complete the level

---
+++ source/core/src/main/com/deco2800/game/components/scores/CalcScore.java
@@ -0,0 +1,26 @@
+package com.deco2800.game.components.scores;
+
+import java.text.DecimalFormat;
+
+public class CalcScore {
+
+    /**
+     * Returns a score based off of the time it took the player to complete the level
+     * If the player does not complete the level before a specific time, their score
+     * will default to 0
+     *
+     * @param clock - in seconds
+     * @return A score which is proportional to the clocks time
+     */
+    public long calculateScore(int clock) {
+        int levelMaxTime = 3 * 60; // The player has to completed the level within 3 minutes
+        int levelScore = 100; // The player will be awarded of 100 points per remaining second
+        int score; // The players calculated score
+
+        int secondsLeftOver = levelMaxTime - clock;
+
+        score = Math.max(0, secondsLeftOver) * levelScore;
+
+        return score;
+    }
+}
