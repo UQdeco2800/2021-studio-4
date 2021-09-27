e1bdd0112254b8541d4608113f8675fef6cb4ed5
Jesse Graf <jesse@grafip.com>
2021-09-27 10:56:50 +1000

    Added a calcScore test class. Also minor bug fixed

--- source/core/src/main/com/deco2800/game/components/scores/CalcScore.java
+++ source/core/src/main/com/deco2800/game/components/scores/CalcScore.java
@@ -10,6 +10,11 @@ public class CalcScore {
      * @return A score which is inversely proportional to the clocks time
      */
     public int calculateScore(int clock) {
+
+        if (clock < 0) {
+            throw new IllegalArgumentException("negative clock value was obtained");
+        }
+
         int levelMaxTime = 3 * 60; // The player has to completed the level within 3 minutes
         int levelScore = 100; // The player will be awarded of 100 points per remaining second
         int score; // The players calculated score
