da76b6c7801c6584a9d555a8b976662d9a3b4331
Jesse Graf <jesse@grafip.com>
2021-10-16 15:58:21 +1000

    Added the virusHead image between level buttons

--- source/core/src/main/com/deco2800/game/components/levelselect/LevelDisplay.java
+++ source/core/src/main/com/deco2800/game/components/levelselect/LevelDisplay.java
@@ -141,10 +141,10 @@ public class LevelDisplay extends UIComponent {
                     break;
             }
 
-            ImageButton imageButton = insImage.setImage(pathName, hoverPathName, posX, posY, widthX, widthY);
-            imageButtons.add(imageButton);
+            ImageButton levelButton = insImage.setImage(pathName, hoverPathName, posX, posY, widthX, widthY);
+            imageButtons.add(levelButton);
 
-            imageButton.addListener(
+            levelButton.addListener(
                 new ChangeListener() {
                     @Override
                     public void changed(ChangeEvent changeEvent, Actor actor) {
@@ -172,12 +172,26 @@ public class LevelDisplay extends UIComponent {
             table.row();
         }
 
+        /**
+         * Creates the button texture for the virus Head.
+         */
+        int posX = centreWidth1;
+        int posY = centreHeight1;
+        int widthX = centreWidth1/4;
+        int widthY = centreHeight1/3;
+        posX = posX - widthX/2;
+        posY = posY - widthY;
+        String virusHeadImage = "player/virus_head_front.png";
+        ImageButton virusHeadBtn;
+        virusHeadBtn = insImage.setImage(virusHeadImage, virusHeadImage, posX, posY, widthX, widthY);
+
         stage.addActor(table);
         for (ImageButton image : imageButtons) {
             stage.addActor(image);
         }
         stage.addActor(exitBtn);
         stage.addActor(titleBtn);
+        stage.addActor(virusHeadBtn);
     }
 
     @Override
