b35a35efa5f5ed84954b3cd5f82597992bdbcf3d
Jesse Graf <jesse@grafip.com>
2021-10-20 12:59:43 +1000

    The rotation of the animation image increases every time you press it.

--- source/core/src/main/com/deco2800/game/components/mainmenu/MainMenuDisplay.java
+++ source/core/src/main/com/deco2800/game/components/mainmenu/MainMenuDisplay.java
@@ -28,11 +28,13 @@ public class MainMenuDisplay extends UIComponent {
     private TitleAnimation titleAnimation;
     private float duration;
     private ImageButton runtimeTitle;
+    private int rotationAmountItr;
 
     /**
      * used tp switch between button states
      */
     public MainMenuDisplay() {
+        rotationAmountItr = 4;
     }
 
     private static final String[] gameTextures = {
@@ -280,6 +282,9 @@ public class MainMenuDisplay extends UIComponent {
                     public void changed(ChangeEvent changeEvent, Actor actor) {
                         logger.info("Launching title Animation");
                         titlesAnimation();
+                        if (rotationAmountItr < 20) {
+                            rotationAmountItr += 2;
+                        }
                     }
                 });
 
@@ -320,7 +325,7 @@ public class MainMenuDisplay extends UIComponent {
 
         titleAnimation = new TitleAnimation(
                 new Texture(imageFile), imageWidth,
-                imageHeight, centreWidth, centreHeight, duration);
+                imageHeight, centreWidth, centreHeight, duration, rotationAmountItr);
 
         //titleAnimationStartTime = ServiceLocator.getTimeSource().getTime();
         titleAnimationStartTime = System.currentTimeMillis();
