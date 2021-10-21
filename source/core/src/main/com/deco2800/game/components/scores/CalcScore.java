d613095f5a67f138f2f543f3b1b340efa007def5
Jesse Graf <jesse@grafip.com>
2021-10-21 14:18:16 +1000

    Fixed changes (minimal)

--- source/core/src/main/com/deco2800/game/components/scores/CalcScore.java
+++ source/core/src/main/com/deco2800/game/components/scores/CalcScore.java
@@ -10,17 +10,15 @@ public class CalcScore {
      * @return A score which is inversely proportional to the clocks time
      */
     public int calculateScore(int clock) {
-
         if (clock < 0) {
             throw new IllegalArgumentException("negative clock value was obtained");
         }
-
         int levelMaxTime = 3 * 60; // The player has to completed the level within 3 minutes
-        int levelScore = 100; // The player will be awarded of 100 points per remaining second
-
+        int levelScore = 100; // The player will be awarded of 100 points/second remaining
         int secondsLeftOver = levelMaxTime - clock;
 
-        // You can't ever get a score of 0. (Since 0 score turns into a '-' when read from textfile)
+        // You can't ever get a score of 0.
+        // (Since 0 score turns into a '-' when read from textfile)
         return Math.max(1, secondsLeftOver) * levelScore;
     }
 }
